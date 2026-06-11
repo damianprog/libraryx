f506ffaa93d840b973bf9494aefd6cc56f6d88c2
damianprog <damianpukszta@gmail.com>
2026-06-11 19:33:49 +0200

    Migrate BooksList to TypeScript

--- src/components/BooksList/BooksList.tsx
+++ src/components/BooksList/BooksList.tsx
@@ -1,17 +1,24 @@
+import type { JSX } from "react";
 import AddIcon from "@mui/icons-material/Add";
 import { useNavigate } from "react-router-dom";
+import type { Book } from "../../types/Book";
 import styles from "./booksList.module.css";
 
-function BooksList({ books, showingUserBooks = false }) {
+type BooksListProps = {
+  books: Book[];
+  showingUserBooks?: boolean;
+};
+
+function BooksList({ books, showingUserBooks = false }: BooksListProps): JSX.Element {
   const navigate = useNavigate();
 
-  const handleResultsRowInfoClick = (book) => {
+  const handleResultsRowInfoClick = (book: Book): void => {
     if (showingUserBooks) {
       navigate("/user-book", { state: book });
     }
   };
 
-  const onResultAdd = (book) => {
+  const onResultAdd = (book: Book): void => {
     navigate("/add-book", { state: book });
   };
 
